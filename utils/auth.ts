c566749c052a4c3c51ba69725737d48e5a3b0f34
Aritra-Dey-117-XT <ece22127@iiitkalyani.ac.in>
2025-01-10 20:24:50 +0530

    Debugging

--- utils/auth.ts
+++ utils/auth.ts
@@ -13,48 +13,67 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   // pages: {
   //   signIn: "/login",
   // },
-  callbacks: {
-    async signIn({ user, profile }) {
+  // callbacks: {
+  //   async signIn({ user, profile }) {
 
-      try {
+  //     try {
 
-        console.log("CONNNECTING TO DB")
-        await connectToDB()
-        console.log("Connected to DB")
-        const isAlreadyExists = await User.findOne({email: user.email}).lean()
-        console.log("CHECKING USER Exists")
+  //       console.log("CONNNECTING TO DB")
+  //       await connectToDB()
+  //       console.log("Connected to DB")
+  //       const isAlreadyExists = await User.findOne({email: user.email}).lean()
+  //       console.log("CHECKING USER Exists")
 
-        if(!isAlreadyExists) {
-          console.log("CReating user")
-          await User.create({
-              id: profile?.sub,
-              name: user?.name,
-              username: profile?.email?.split("@")[0],
-              email: user?.email,
-              password: "GooglePassword",
-              imageURL: user?.image
-            })
-        }
-        return true
+  //       if(!isAlreadyExists) {
+  //         console.log("CReating user")
+  //         await User.create({
+  //             id: profile?.sub,
+  //             name: user?.name,
+  //             username: profile?.email?.split("@")[0],
+  //             email: user?.email,
+  //             password: "GooglePassword",
+  //             imageURL: user?.image
+  //           })
+  //       }
+  //       return true
 
-      } catch (error) {
-        console.log("Error During SignIn: " + error)
-        return false
-      }
+  //     } catch (error) {
+  //       console.log("Error During SignIn: " + error)
+  //       return false
+  //     }
       
+  //   },
+  //   async jwt({ token, account, profile }) {
+  //     // Persist the OAuth access_token and or the user id to the token right after signin
+  //     if (account) {
+  //       token.id = profile?.sub
+  //     }
+  //     return token
+  //   },
+  //   async session({ session, token }) {
+  //     // Send properties to the client, like an access_token and user id from a provider.
+  //     session.user.id = token.id as string
+      
+  //     return session
+  //   }
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      console.log("SignIn Callback - User:", user);
+      console.log("SignIn Callback - Account:", account);
+      console.log("SignIn Callback - Profile:", profile);
+      return true; // or add conditional checks here
     },
     async jwt({ token, account, profile }) {
-      // Persist the OAuth access_token and or the user id to the token right after signin
-      if (account) {
-        token.id = profile?.sub
-      }
-      return token
+      console.log("JWT Callback - Token:", token);
+      console.log("JWT Callback - Account:", account);
+      return token;
     },
     async session({ session, token }) {
-      // Send properties to the client, like an access_token and user id from a provider.
-      session.user.id = token.id as string
-      
-      return session
-    }
-  }
+      console.log("Session Callback - Session:", session);
+      console.log("Session Callback - Token:", token);
+      return session;
+    },
+  },
+  
+  // }
 })
